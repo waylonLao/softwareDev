07fcaff412bd656151e4a96bc1cb5fa8d551b8d2
Joshua Tran <sorrowfeed@gmail.com>
2016-10-24 12:09:12 -0400

    weapon and tool classes added by Josh

---
+++ src/MainCode/Tool.java
@@ -0,0 +1,10 @@
+package MainCode;
+
+public class Tool extends Item{
+
+	public Tool(int itemID) {
+		super(itemID);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/MainCode/Weapon.java
@@ -0,0 +1,10 @@
+package MainCode;
+
+public class Weapon extends Item{
+
+	public Weapon(int itemID) {
+		super(itemID);
+		// TODO Auto-generated constructor stub
+	}
+
+}
