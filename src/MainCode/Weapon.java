c476679957c929948316a2ade72ef579b29fbfc6
nklemenc <nklemenc@ggc.edu>
2016-10-25 20:51:37 -0400

    Add files via upload
    Updated Weapon.java

--- src/MainCode/Weapon.java
+++ src/MainCode/Weapon.java
@@ -1,10 +1,30 @@
 package MainCode;
+/**
+* @author nklemenc
+*/
+public class Weapon extends Item {
+	private int weaponDamage;
 
-public class Weapon extends Item{
-
-	public Weapon(int itemID) {
+	public Weapon(int itemID, int weaponDmg) {
 		super(itemID);
+		weaponDamage = weaponDmg;
 		// TODO Auto-generated constructor stub
 	}
+
+	/**
+	 * @return the weaponDamage
+	 */
+	public int getWeaponDamage() {
+		return weaponDamage;
+	}
+
+	/**
+	 * @param weaponDamage the weaponDamage to set
+	 */
+	public void setWeaponDamage(int weaponDamage) {
+		this.weaponDamage = weaponDamage;
+	}
+	
+	
 
 }
