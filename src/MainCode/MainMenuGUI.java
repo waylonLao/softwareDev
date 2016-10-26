32fd814271379d8f9b27e5796c84ce399dcf0269
Joshua Tran <sorrowfeed@gmail.com>
2016-10-26 15:07:01 -0400

    updated MainMenuGUI -Josh

--- src/MainCode/MainMenuGUI.java
+++ src/MainCode/MainMenuGUI.java
@@ -1,11 +1,26 @@
 package MainCode;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class MainMenuGUI extends Application{
@@ -37,7 +52,21 @@ public class MainMenuGUI extends Application{
 		//create StackPane
 		StackPane stackPane1 = new StackPane();
 		
+		//borderPane
+		BorderPane border = new BorderPane();
+		HBox hbox = addHBox();
+		border.setTop(hbox);
+		border.setLeft(addVBox());
+		addStackPane(hbox);         // Add stack to HBox in top region
+
+		border.setCenter(addGridPane());
+		border.setRight(addFlowPane());
+		/*
 		//stackpane setup
+		StackPane.setMargin(labelTitle, new Insets(10,10,10,10));
+		StackPane.setAlignment(labelTitle, Pos.TOP_CENTER);
+		StackPane.setAlignment(b1, Pos.TOP_CENTER);
+		
 		stackPane1.getChildren().add(labelTitle);
 		stackPane1.getChildren().add(b1);
 		
@@ -47,10 +76,68 @@ public class MainMenuGUI extends Application{
 		//stage setup
 		mainMenuStage.setTitle("Galaxy Explorer");
 		mainMenuStage.setScene(mainMenuScene);
-		mainMenuStage.show();
+		mainMenuStage.show();*/
 	}
-		
-		 public static void main(String[] args)
+	private Node addFlowPane() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	private Node addGridPane() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public void addStackPane(HBox hb) {
+	    StackPane stack = new StackPane();
+	    Rectangle helpIcon = new Rectangle(30.0, 25.0);
+	    helpIcon.setFill(new LinearGradient(0,0,0,1, true, CycleMethod.NO_CYCLE,
+	        new Stop[]{
+	        new Stop(0,Color.web("#4977A3")),
+	        new Stop(0.5, Color.web("#B0C6DA")),
+	        new Stop(1,Color.web("#9CB6CF")),}));
+	    helpIcon.setStroke(Color.web("#D0E6FA"));
+	    helpIcon.setArcHeight(3.5);
+	    helpIcon.setArcWidth(3.5);
+
+	    Text helpText = new Text("?");
+	    helpText.setFont(Font.font("Verdana", FontWeight.BOLD, 18));
+	    helpText.setFill(Color.WHITE);
+	    helpText.setStroke(Color.web("#7080A0")); 
+
+	    stack.getChildren().addAll(helpIcon, helpText);
+	    stack.setAlignment(Pos.CENTER_RIGHT);     // Right-justify nodes in stack
+	    StackPane.setMargin(helpText, new Insets(0, 10, 0, 0)); // Center "?"
+
+	    hb.getChildren().add(stack);            // Add to HBox from Example 1-2
+	    HBox.setHgrow(stack, Priority.ALWAYS);    // Give stack any extra space
+	}
+	public VBox addVBox() {
+		    VBox vbox = new VBox();
+		    vbox.setPadding(new Insets(10));
+		    vbox.setSpacing(8);
+
+		    Text title = new Text("Data");
+		    title.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+		    vbox.getChildren().add(title);
+		return vbox;
+	}
+
+	public HBox addHBox() {
+	    HBox hbox = new HBox();
+	    hbox.setPadding(new Insets(15, 12, 15, 12));
+	    hbox.setSpacing(10);
+	    hbox.setStyle("-fx-background-color: #336699;");
+
+	    Button buttonCurrent = new Button("Current");
+	    buttonCurrent.setPrefSize(100, 20);
+
+	    Button buttonProjected = new Button("Projected");
+	    buttonProjected.setPrefSize(100, 20);
+	    hbox.getChildren().addAll(buttonCurrent, buttonProjected);
+
+	    return hbox;
+	}
+
+		public static void main(String[] args)
 		 {
 		        launch(args);
 		 }
