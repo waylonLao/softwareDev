5982813c82af35309b0ec3c3d1e2da05f71d3580
nklemenc <nklemenc@ggc.edu>
2016-10-25 21:00:12 -0400

    created DamageTester

---
+++ src/MainCode/DamageTester.java
@@ -0,0 +1,20 @@
+package MainCode;
+/**
+* @author nklemenc
+*/
+public class DamageTester {
+
+
+	public static void main(String[] args) {
+		
+		
+		Item testItem = new Item(1);
+		Weapon testWeapon = new Weapon(1, 25);
+		
+		Player testPlayer = new Player(200, testWeapon);
+		Monster testMonster = new Monster(200, testWeapon, 1, "Test Monster", "This is a test Monster", testItem);
+		
+		testPlayer.doDamage(testMonster);
+	}
+
+}
