9fd496f73c14ed5255f5f7e117cbf87c2316a272
Joshua Tran <sorrowfeed@gmail.com>
2016-10-26 10:42:58 -0400

    updated tool by Josh

--- src/MainCode/Tool.java
+++ src/MainCode/Tool.java
@@ -2,9 +2,18 @@ package MainCode;
 
 public class Tool extends Item{
 
+	private int toolID;
+
 	public Tool(int itemID) {
 		super(itemID);
 		// TODO Auto-generated constructor stub
 	}
 
+	public int getToolID() {
+		return toolID;
+	}
+
+	public void setToolID(int toolID) {
+		this.toolID = toolID;
+	}
 }
