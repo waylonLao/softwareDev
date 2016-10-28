3c4a5e1317e72d36340d349986d5053fc864da9a
Joshua Tran <sorrowfeed@gmail.com>
2016-10-28 10:41:17 -0400

    Josh

--- src/MainCode/ItemFactory.java
+++ src/MainCode/ItemFactory.java
@@ -1,6 +1,21 @@
 package MainCode;
 
+import java.util.Random;
+
 public class ItemFactory implements ItemInterface {
 
-	if()
+	public Item getItemName(){
+	Random number = new Random(4);
+	
+	if(number <= 2)
+	{
+		return new HealthKit(1);
+	}
+	else if(number >= 3)
+	{
+		return new ConcussionGrenade(2);
+	}
+	
+	return null;
+	}
 }
