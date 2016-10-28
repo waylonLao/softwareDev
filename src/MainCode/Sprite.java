c756ac357c437380d925ac1d22099a6da10d7f75
nklemenc <nklemenc@ggc.edu>
2016-10-27 22:27:18 -0400

    updated Sprite

--- src/MainCode/Sprite.java
+++ src/MainCode/Sprite.java
@@ -5,6 +5,7 @@ package MainCode;
 public class Sprite {
 	
 	private int health;
+	private int maxHealth;
 	private Weapon weaponID;
 	private String name;
 	
@@ -15,9 +16,28 @@ public class Sprite {
 	 * @param weaponID
 	 */
 	public Sprite(int h, Weapon w, String n) {
+		maxHealth = h;
 		health = h;
 		weaponID = w;
 		name = n;
+	}
+
+
+
+	/**
+	 * @return the maxHealth
+	 */
+	public int getMaxHealth() {
+		return maxHealth;
+	}
+
+
+
+	/**
+	 * @param maxHealth the maxHealth to set
+	 */
+	public void setMaxHealth(int maxHealth) {
+		this.maxHealth = maxHealth;
 	}
 
 
@@ -76,15 +96,22 @@ public class Sprite {
 
 
 
-	public void doDamage(Sprite target){
+	public String doDamage(Sprite target){
 		target.setHealth(target.getHealth() - this.getWeaponID().getWeaponDamage());
-		System.out.println(this.getName() + " dealt " + this.getWeaponID().getWeaponDamage() + " damage to the " + target.getName());
-		System.out.println("The " + target.getName() + " now has " + target.getHealth() + " health ");
 		if(target.getHealth() <= 0){
-			System.out.println(target.getName() + " has been defeated!");
+			return target.getName() + " has been defeated!";
 		}
+		return this.getName() + " dealt " + this.getWeaponID().getWeaponDamage() + " damage to the " + target.getName();
 		
 		
+	}
+	
+	public String takeDamage(Sprite target){
+		this.setHealth(this.getHealth() - target.getWeaponID().getWeaponDamage());
+		if(this.getHealth() <= 0){
+			return "You died.";
+		}
+		return target.getName() + " has dealt " + target.getWeaponID().getWeaponDamage() + " damage to you ";
 	}
 
 }
