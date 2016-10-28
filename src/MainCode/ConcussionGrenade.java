da49b84531818b0687ade61a6a3d095fd5274c9b
Joshua Tran <sorrowfeed@gmail.com>
2016-10-28 09:58:35 -0400

    -Josh

--- src/MainCode/ConcussionGrenade.java
+++ src/MainCode/ConcussionGrenade.java
@@ -1,5 +1,20 @@
 package MainCode;
 
-public class ConcussionGrenade {
+public class ConcussionGrenade extends Item implements ItemInterface{
+
+	public ConcussionGrenade(int itemID) {
+		super(itemID);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Item createConcussionGrenade()
+	{
+		Item grenade = new Item(2);
+		grenade.setItemName("Concussion Grenade");
+		grenade.setItemDescription("A powerful stun grenade");
+		grenade.setItemUse("Use to stun monster for a turn");
+		return null;
+		
+	}
 
 }
--- src/MainCode/HealthKit.java
+++ src/MainCode/HealthKit.java
@@ -1,13 +1,18 @@
 package MainCode;
 
-public class HealthKit implements ItemInterface {
+public class HealthKit extends Item implements ItemInterface {
 	
-	public void createHealthKit()
+	public HealthKit(int itemID) {
+		super(itemID);
+	}
+
+	public Item createHealthKit()
 	{
 		Item consumable = new Item(1);
 		consumable.setItemName("Health Kit");
 		consumable.setItemDescription("A white box with a swiss army cross on it");
 		consumable.setItemUse("Use to heal yourself for 50 HP");
+		return consumable;
 		
 	}
 }
