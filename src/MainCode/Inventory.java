e26babe843b1ee7490425247b389493295122d56
Joshua Tran <sorrowfeed@gmail.com>
2016-11-02 10:16:07 -0400

    fixed inventory -josh

--- src/MainCode/Inventory.java
+++ src/MainCode/Inventory.java
@@ -10,35 +10,42 @@ import java.util.ArrayList;
  */
 public class Inventory {
     
-    private ArrayList keyItemInventory = new ArrayList<Item>();
-    private ArrayList consumableInventory = new ArrayList<Item>();
+    private ArrayList<Item> keyItemInventory = new ArrayList<Item>();
+    private ArrayList<Item> consumableInventory = new ArrayList<Item>();
 
     /**
      * @return the keyItemInventory
      */
-    public ArrayList getKeyItemInventory() {
+    public ArrayList<Item> getKeyItemInventory() {
         return keyItemInventory;
     }
 
     /**
      * @param keyItemInventory the keyItemInventory to set
      */
-    public void setKeyItemInventory(ArrayList keyItemInventory) {
+    public void setKeyItemInventory(ArrayList<Item> keyItemInventory) {
         this.keyItemInventory = keyItemInventory;
     }
 
     /**
      * @return the consumableInventory
      */
-    public ArrayList getConsumableInventory() {
+    public ArrayList<Item> getConsumableInventory() {
         return consumableInventory;
     }
 
     /**
      * @param consumableInventory the consumableInventory to set
      */
-    public void setConsumableInventory(ArrayList consumableInventory) {
+    public void setConsumableInventory(ArrayList<Item> consumableInventory) {
         this.consumableInventory = consumableInventory;
     }
-    
+    public void addItem(Item item)
+    {
+    	consumableInventory.add(item);
+    }
+    public void addKeyItem(Item item)
+    {
+    	keyItemInventory.add(item);
+    }
 }
