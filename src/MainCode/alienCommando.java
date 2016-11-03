5cdba95adbbd0635c3b82f4dfc84fbaa875117dc
Joshua Tran <sorrowfeed@gmail.com>
2016-11-03 14:01:30 -0400

    Update alienCommando.java
    You need to change the Monster constructor into a no param constructor and then you need create a no param constructor for AlienCommando

--- src/MainCode/alienCommando.java
+++ src/MainCode/alienCommando.java
@@ -2,7 +2,8 @@ package MainCode;
 
 public class alienCommando extends Monster implements monsterInterface
 {
-
+	//your constructors need to be no param constructor
+	//you have to hard code all of these variables using setters into this specific class
 	public alienCommando(int health, Weapon wpn, int monsterID, String monsterName, String monsterDescription, Item itemDrop) {
 		super(monsterID, wpn, monsterID, monsterName, 
 				monsterDescription, itemDrop);
