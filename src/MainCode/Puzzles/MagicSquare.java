0a6758e1aa1c11e3c77a30dfb9aaf6e978c74360
Main <Main>
2016-11-10 22:29:20 -0500

    Magic Square puzzle

--- src/MainCode/Puzzles/MagicSquare.java
+++ src/MainCode/Puzzles/MagicSquare.java
@@ -13,6 +13,15 @@ public class MagicSquare extends Puzzle{
 	public MagicSquare()
 	{
 		setPuzzleName("Magic Square");
+		setPuzzleDescription("Fill in the blanks so that each row and column adds up to 15.\n "
+				+ "------------\n" + "| 6 |   | 8 |\n" + "|   | 5 |   |\n" + "| 2 |   | 4 |\n"
+				+ "------------\n" + "Insert numbers from top to bottom, left to right");
+	}
+	
+	public static void main(String[] args)
+	{
+		MagicSquare ms = new MagicSquare();
+		System.out.println(ms);
 	}
 
 }
