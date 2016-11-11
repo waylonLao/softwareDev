92590d7864e59cc64a132f349fea4dc3a3f8d8bc
afirdaus <afirdaus@ggc.edu>
2016-11-10 21:35:25 -0500

    rids

---
+++ src/MainCode/Puzzles/AIPuzzle.java
@@ -0,0 +1,29 @@
+/**
+ * Class: AIPuzzle.java
+ * 
+ * @author afirdaus
+ * @version 1.0
+ *          <p>
+ * Course : ITEC 2150 Fall 2014 
+ * Written: Nov 10, 2016
+ * 
+ * 
+ *  This class – ******
+ * 
+ *  Purpose: – ******
+ */
+package MainCode.Puzzles;
+
+public class AIPuzzle extends RiddlePuzzle
+
+{
+	public AIPuzzle() 
+	{
+		setPuzzleID(7);
+		setPuzzleDescription("Who is the test for AI"
+				+ "in a computer system named for?");
+		setPuzzleSolution("Turing");
+
+	}
+
+}
