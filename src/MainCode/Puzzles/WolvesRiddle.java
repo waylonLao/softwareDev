64d3e76271d2b09d8dfe68097b14ec9a56946532
afirdaus <afirdaus@ggc.edu>
2016-11-10 21:16:09 -0500

    wolves

---
+++ src/MainCode/Puzzles/WolvesRiddle.java
@@ -0,0 +1,31 @@
+/**
+ * Class: WolvesRiddle.java
+ * 
+ * @author afirdaus
+ * @version 1.0
+ *          <p>
+ * Course : ITEC 2150 Fall 2014 
+ * Written: Nov 10, 2016
+ * 
+ * 
+ *  This class – ******
+ * 
+ *  Purpose: – ******
+ */
+package MainCode.Puzzles;
+
+public class WolvesRiddle extends RiddlePuzzle
+
+{
+	public WolvesRiddle() 
+	{
+		setPuzzleID(3);
+		//setPuzzleName("");
+		setPuzzleDescription("Six wolves catch six lambs in six minutes. "
+				+ " How many wolves will be needed to catch sixty lambs in "
+				+ "sixty minutes? ");
+		setPuzzleSolution("6");
+	}
+	
+	
+}
